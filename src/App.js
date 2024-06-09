1132d3eb147061910207f4f9996fb9f232743a82
Magnific86 <112124588+Magnific86@users.noreply.github.com>
2024-06-09 21:59:16 +0300

    q

--- src/App.js
+++ src/App.js
@@ -1,39 +1,47 @@
-import React, { useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import { MyModal } from "./MyModal"
 import { STRIP_COST, koeffsArr } from "./data"
 
 export const App = () => {
   const [plotLength, setPlotLength] = useState(0)
   const [stripCount, setStripCount] = useState(0)
-
   const [isActive, setIsActive] = useState(false)
-
   const [koeffs, setKoeffs] = useState({})
+  const [result, setResult] = useState(0)
+
+  const resetKoeffsToDefault = () => {
+    koeffsArr.forEach(k => setKoeffs(prev => ({ ...prev, [k.id]: k.defaultValue })))
+  }
 
   useEffect(() => {
-    koeffsArr.forEach(k => setKoeffs(prev => ({ ...prev, [k.id]: k.defaultValue })))
+    resetKoeffsToDefault()
   }, [])
-
-  const [result, setResult] = useState(0)
 
   const submitHandler = e => {
     e.preventDefault()
 
-    let result = plotLength * stripCount * STRIP_COST
-
     const layersK6 = koeffs["layer"]
+
+    console.log("layersK6", layersK6)
+
+    if (layersK6 === null) {
+      alert("Введите кол-во слоев")
+      return
+    }
+
+    let result = plotLength * stripCount * STRIP_COST
 
     let otherKoeffs = 0
 
     koeffsArr.forEach(k => {
-      if (k.id === "layer") {
-      } else {
+      if (k.id !== "layer" && !!koeffs[k.id]) {
         otherKoeffs += koeffs[k.id] / 100
       }
     })
 
-    console.log("otherKoeffs", otherKoeffs)
-    console.log("layersK6", layersK6)
+    console.log("result", result)
+    console.log("1 - layersK6", 1 - layersK6)
+    console.log("1 - otherKoeffs", 1 - otherKoeffs)
 
     setResult(result * (1 - layersK6) * (1 - otherKoeffs))
   }
@@ -41,7 +49,8 @@ export const App = () => {
   const resetHandler = () => {
     setPlotLength(0)
     setStripCount(0)
-    setKoeffs({})
+    resetKoeffsToDefault()
+    setResult(0)
   }
 
   return (
@@ -51,23 +60,16 @@ export const App = () => {
           Длина участка
           <input type="number" value={plotLength} onChange={e => setPlotLength(e.target.value)} />
         </label>
-        {/* <label>
-          Стоимость полосы
-          <input type="number" value={stripCost} onChange={e => setStripCost(e.target.value)} />
-        </label> */}
-
         <label>
           Кол-во полос
           <input type="number" value={stripCount} onChange={e => setStripCount(e.target.value)} />
         </label>
-        <label>Стоимость полосы: {STRIP_COST}</label>
+        <label>Стоимость полосы: {STRIP_COST} (для упрощения пока 1 поставил, чтобы такие большие суммы не были потом верну)</label>
         <button>Посчитать</button>
       </form>
       {result && <h1>Результат: {result} миллионов</h1>}
-
       <MyModal setK={setKoeffs} kData={koeffs} isActive={isActive} setIsActive={setIsActive} />
-
-      <button onClick={resetHandler}>Сбросить все пока не работает кнопка</button>
+      <button onClick={resetHandler}>Сбросить все</button>
     </div>
   )
 }
--- src/MyModal.js
+++ src/MyModal.js
@@ -1,52 +1,63 @@
-import React, { useEffect, useRef, useState } from "react"
-import { koeffsArr, layersData } from "./data"
+import { useEffect, useRef, useState } from "react"
+import { koeffsArr, layersArr, layersData } from "./data"
 import useOnClickOutside from "./utils/useClickOutside"
 
 export const MyModal = ({ setIsActive, isActive, kData, setK }) => {
+  const [activeLayer, setActiveLayer] = useState(null)
+
   const modalRef = useRef(null)
 
   useOnClickOutside(modalRef, () => setIsActive(false))
-
-  const [activeLayer, setActiveLayer] = useState(-1)
 
   useEffect(() => {
     setK({ ...kData, layer: layersData[activeLayer] })
   }, [activeLayer])
 
+  useEffect(() => {
+    if (!!kData["layer"]) {
+      setActiveLayer(kData["layer"])
+    }
+
+    return () => setActiveLayer(null)
+  }, [])
+
   return (
     <>
       <button onClick={() => setIsActive(true)}>Редактировать коэффициенты</button>
-      <div ref={modalRef}>
-        {isActive && (
-          <div
-            className="modal-container"
-            onClick={e => {
-              e.stopPropagation()
-              console.log("тыккк")
-            }}
-          >
+      {isActive && (
+        <div className="modal-wrapper">
+          <div ref={modalRef} className="modal-container" onClick={e => e.stopPropagation()}>
             {koeffsArr.map(k => (
-              <label key={k.id}>
-                {k.label}
-                <input type="number" value={kData[k.id]} onChange={e => setK({ ...kData, [k.id]: e.target.value })} />
+              <div key={k.id}>
+                <label>
+                  Учитывать
+                  <input
+                    type="checkbox"
+                    checked={!!kData[k.id]}
+                    onChange={e => {
+                      const newObj = { ...kData }
+                      if (e.target.checked) {
+                        setK(newObj)
+                      } else {
+                        delete newObj[k.id]
+                        setK(newObj)
+                      }
+                    }}
+                  />
+                </label>
+                <label>
+                  {k.label}
+                  <input type="number" value={kData[k.id]} onChange={e => setK({ ...kData, [k.id]: e.target.value })} />
+                </label>
+              </div>
+            ))}
+            <p>Выберите кол-во слоев, это 6 kэф:</p>
+            {layersArr.map(layer => (
+              <label key={layer.value}>
+                {layer.label}
+                <input type="radio" checked={activeLayer === layer.value} onChange={() => setActiveLayer(layer.value)} />
               </label>
             ))}
-
-            <p>Выберите колво слоев, это 6 kэф:</p>
-
-            <label>
-              0 слоев
-              <input type="radio" checked={activeLayer === "zero"} onChange={() => setActiveLayer("zero")} />
-            </label>
-            <label>
-              1 слой
-              <input type="radio" checked={activeLayer === "one"} onChange={() => setActiveLayer("one")} />
-            </label>
-            <label>
-              2 слоя
-              <input type="radio" checked={activeLayer === "two"} onChange={() => setActiveLayer("two")} />
-            </label>
-
             <button
               onClick={e => {
                 e.stopPropagation()
@@ -56,8 +67,8 @@ export const MyModal = ({ setIsActive, isActive, kData, setK }) => {
               Сохранить
             </button>
           </div>
-        )}
-      </div>
+        </div>
+      )}
     </>
   )
 }
